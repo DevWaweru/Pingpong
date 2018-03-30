985a0306ceec60d5a8abce22c2d2e0c466cabb6c
Waweru <developer.waweru@gmail.com>
2018-03-30 13:39:42 +0300

    Adding background image on the header section

--- js/scripts.js
+++ js/scripts.js
@@ -34,7 +34,6 @@ $(document).ready(function () {
             }
         }
     });
-
     // Closes responsive menu when a scroll trigger link is clicked
     $('.js-scroll-trigger').click(function () {
         $('.navbar-collapse').collapse('hide');
